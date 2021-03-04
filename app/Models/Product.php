d89b043bda38b0bd9d42ee1f45ba806248f2df80
Nason <878826676@qq.com>
2021-03-04 18:20:06 +0800

    商品模型

---
+++ app/Models/Product.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title', 'description', 'image', 'on_sale',
+        'rating', 'sold_count', 'review_count', 'price'
+    ];
+
+    protected $casts = [
+        'on_sale' => 'boolean', // on_sale 是一个布尔类型的字段
+    ];
+
+    // 与商品 sku 关联
+    public function skus()
+    {
+        return $this->hasMany(ProductSku::class);
+    }
+}
