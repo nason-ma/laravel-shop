7c66d58e65c372e775601110b5618c5bb57c21f3
Nason <878826676@qq.com>
2021-03-05 10:32:48 +0800

    用户界面 - 商品筛选、排序

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -3,9 +3,32 @@
 
 @section('content')
     <div class="row">
-        <div class="col-lg-10 col-lg-offset-1">
+        <div class="col-lg-10 offset-lg-1">
             <div class="card">
                 <div class="card-body">
+                    <!-- 筛选组件开始 -->
+                    <form action="{{ route('products.index') }}" class="search-form">
+                        <div class="form-row">
+                            <div class="col-md-9">
+                                <div class="form-row">
+                                    <div class="col-auto"><input type="text" class="form-control form-control-sm" name="search" value="{{ $filters['search'] }}" placeholder="搜索"></div>
+                                    <div class="col-auto"><button class="btn btn-primary btn-sm">搜索</button></div>
+                                </div>
+                            </div>
+                            <div class="col-md-3">
+                                <select name="order" class="form-control form-control-sm float-right">
+                                    <option value="">排序方式</option>
+                                    <option value="price_asc">价格从低到高</option>
+                                    <option value="price_desc">价格从高到低</option>
+                                    <option value="sold_count_desc">销量从高到低</option>
+                                    <option value="sold_count_asc">销量从低到高</option>
+                                    <option value="rating_desc">评价从高到低</option>
+                                    <option value="rating_asc">评价从低到高</option>
+                                </select>
+                            </div>
+                        </div>
+                    </form>
+                    <!-- 筛选组件结束 -->
                     <div class="row products-list">
                         @foreach($products as $product)
                             <div class="col-3 product-item">
@@ -23,9 +46,24 @@
                             </div>
                         @endforeach
                     </div>
-                    <div class="float-right">{{ $products->render() }}</div>
+                    <div class="float-right">{{ $products->appends($filters)->render() }}</div>
                 </div>
             </div>
         </div>
     </div>
+@endsection
+
+@section('scriptsAfterJs')
+    <script>
+        // 把控制器传过来的 $filters 变量变成一个 JSON 字符串
+        var filters = {!! json_encode($filters) !!};
+        $(document).ready(function () {
+            // $('.search-form input[name=search]').val(filters.search);
+            var selectOrderObj = $('.search-form select[name=order]');
+            selectOrderObj.val(filters.order);
+            selectOrderObj.on('change', function () {
+                $('.search-form').submit();
+            });
+        })
+    </script>
 @endsection
