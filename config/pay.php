94743830d26bc9bbfe11a6dfa7d37f112e9cd3cb
Nason <878826676@qq.com>
2021-03-15 21:06:16 +0800

    集成支付宝

--- config/pay.php
+++ config/pay.php
@@ -31,9 +31,9 @@
 
 return [
     'alipay' => [
-        'app_id' => '',
-        'ali_public_key' => '',
-        'private_key' => '',
+        'app_id' => '2021000117622412',
+        'ali_public_key' => 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAt9irmrjdMHjA1T/wnBkWBH6YI5drjGKgEue76DYsp6/7k+7yOSYUP+oCCwQ44yg//k9cxe50sl+OAyDxKOWXrXZI8Yj/jieVIBTOvHdsc3YF7kGNz3qvyjm5NVR0FJCw2+SIG09SwnmMx6Q4ZaCbMzw08LLVizPmZNabeT749SxS+7DswI9TPvFsePASdwP089fYhoxrXRteLvqLsSCkoYFEscsAahOTFmAnfGvMl/fIbY4NsG3UrMI7WTV6hH2v9dJiwLG0dxPDawmdOdBhSHLT0T8H1+r5ye2o5v6cK5Mwpekrtuf4JqsXaM/VioAYHtCZe4RhksERWKGgxPCMQQIDAQAB',
+        'private_key' => 'MIIEpAIBAAKCAQEAiibnOubjQjDP3f72jwXGU60gJFWVpWgDynbLHHAa8hg1L6h+CfgwbzdPaE7t5vFtE7mNn0cjwDb3m8DH/ZveeFbr/H3wWDWvj0kBMQdxOzz8IigPkA73Wo1wwxmcFYIwhLBYWQ5jzuykaFwxSxK8nweV5KygDd7y8DjRNFYBQEUAHkDxjH/PhMCTRIvDrnjNOaQc6WGK8KHTlyFE4g5c//8la+hRtC5vKwtVt6jDLJRSg6ln8X1JsJoDYieo5s7i+t17gTh+8/RrXmRoW42CJCGWWi39dn+GXZv4gQkUxnZ8mCzLtYjAGlWmuCfG0JhF5JXbdBOOyurWXQyKFhoSewIDAQABAoIBAGLIcHxojcVNe8TDw27+9RHt79arL6anGlbR3GfILSxAbP9X3k+onJFZnjj6fxesN49sFTz3tDOD2kcPcLEBm/C2TmUrdAjO0mqFDAoPxUJTWAHgXUhnuyyPiDqo/DdQ8Ppf0bxUEr/p66EHkuOYUTDxTpfFa2ifq1xjdAuVF63o21QubxC8z0Dhyg9xTuyhce0bWXRnQLuhnLXecoxHyv4Ow7VTX0igXYYg4N9flB9bulw1u7OmGCwWaTJq6tZTqMX7cENo6zImwwI/ratrhNltQE3HnFDKJX+TSI1WvajFcEpzyoofHIta2ezP4coxr+/7SLXZoHLLfwhB+VtquokCgYEAxrI9B5bobkEiUM4QnXDdgLE0FnmvKOt/nvMolqhBVq7tRiETcmKRtKQwPDLwSozzc/hgl4wJq3V4cbehKNdrAJftPlLv3zlh8JEoxMi1OlxffSgrtNmpUSwzuMU0EItlNPnFzrt4d42dzrpqfQzY5krHLkKV7mvtL8LMk1laxP0CgYEAsf6s4zTIOW9yhJoGzBG8rLSX3nLiIOLA18GtmzG9u3r3dGGwKdE306uMY6erkKDPKJbl71NVA/pLITN0OEJQLy/SAAjfT6FjXb/k5JqtfjPhckY3hnIdJI9OTJcTXu32cgGp9tTaD1EuOufLtQNd29ov6d1BY+H/nOmfCbPbytcCgYEAvql0BJ9bQ8j+i7lgmiMeeYoQ6Gw1lzd7i+Pak866e9p5Vm0Qwxcwp71Rh8J9b5wXVLYGwgUflkavn7dMTut/At/GSKK/CF2AzJ4OF95cgLb47hko725Zmcmni+q3jhSkthPjKwAbleTvg9q/qmU0kK86t/ou8szfmY/8THxWrGUCgYBrUKo6HoOtpVT9P8RjWzpPQl7iPo+51h0AuIQMMtQ66euyHofQ3pRzKVXua5QUEfB0f25Iegyi5lBPxy8GEG051fIeFTtWTm6TgnPlpKB/m5YMiOtHQxvxA/noUZnbPUu99uTbrf9/BqoB1+lIzKvVNtp2fPDE4Oh1WaGKfwdAZwKBgQC45BVKi9q1LeQ1SaOHIqC/HakGhSE5ZRc+qEMWhFQIjE8+NMgrh7Oi7IQlFkziEPCehpGp6z4v+ZewuqD/BtoajvKPSMsO+ly5UU0XMafTjKZhyS+1tTvWe9IqJ+QoPose/LPQ8T40GRWUJfOp7fHIfSRGRHW3U+UkmbiGDIPnyw==',
         'log' => [
             'file' => storage_path('logs/alipay.log'),
         ],
