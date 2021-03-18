5cae0646f9c73bcfd8436b1a28d2e61b8fe49d3d
Nason <878826676@qq.com>
2021-03-18 17:10:15 +0800

    管理后台 - 拒绝退款

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -25,4 +25,5 @@ Route::group([
     $router->get('orders', 'OrdersController@index')->name('orders.index');
     $router->get('orders/{order}', 'OrdersController@show')->name('orders.show');
     $router->post('orders/{order}/ship', 'OrdersController@ship')->name('orders.ship');
+    $router->post('orders/{order}/refund', 'OrdersController@handleRefund')->name('orders.handle_refund');
 });
--- resources/views/orders/show.blade.php
+++ resources/views/orders/show.blade.php
@@ -104,28 +104,34 @@
                                         未支付
                                     @endif
                                 </div>
-                                <!-- 支付按钮开始 -->
-                                @if(!$order->paid_at && !$order->closed)
-                                    <div class="payment-buttons">
-                                        <a class="btn btn-primary btn-sm"
-                                           href="{{ route('payment.alipay', ['order' => $order->id]) }}">支付宝支付</a>
-                                        {{--<button class="btn btn-sm btn-success" id='btn-wechat'>微信支付</button>--}}
-                                    </div>
-                                @endif
-                                <!-- 如果订单的发货状态为已发货则展示确认收货按钮 -->
-                                @if($order->ship_status === \App\Models\Order::SHIP_STATUS_DELIVERED)
-                                    <div class="receive-button">
-                                        <button type="button" id="btn-receive" class="btn btn-sm btn-success">确认收货
-                                        </button>
-                                    </div>
-                                @endif
-                                <!-- 订单已支付，且退款状态是未退款时展示申请退款按钮 -->
-                                @if($order->paid_at && $order->refund_status === \App\Models\Order::REFUND_STATUS_PENDING)
-                                    <div class="refund-button">
-                                        <button class="btn btn-sm btn-danger" id="btn-apply-refund">申请退款</button>
-                                    </div>
-                                @endif
                             </div>
+                            @if(isset($order->extra['refund_disagree_reason']))
+                                <div>
+                                    <span>拒绝退款理由：</span>
+                                    <div class="value">{{ $order->extra['refund_disagree_reason'] }}</div>
+                                </div>
+                            @endif
+                            <!-- 支付按钮开始 -->
+                            @if(!$order->paid_at && !$order->closed)
+                                <div class="payment-buttons">
+                                    <a class="btn btn-primary btn-sm"
+                                       href="{{ route('payment.alipay', ['order' => $order->id]) }}">支付宝支付</a>
+                                    {{--<button class="btn btn-sm btn-success" id='btn-wechat'>微信支付</button>--}}
+                                </div>
+                            @endif
+                            <!-- 如果订单的发货状态为已发货则展示确认收货按钮 -->
+                            @if($order->ship_status === \App\Models\Order::SHIP_STATUS_DELIVERED)
+                                <div class="receive-button">
+                                    <button type="button" id="btn-receive" class="btn btn-sm btn-success">确认收货
+                                    </button>
+                                </div>
+                            @endif
+                            <!-- 订单已支付，且退款状态是未退款时展示申请退款按钮 -->
+                            @if($order->paid_at && $order->refund_status === \App\Models\Order::REFUND_STATUS_PENDING)
+                                <div class="refund-button">
+                                    <button class="btn btn-sm btn-danger" id="btn-apply-refund">申请退款</button>
+                                </div>
+                            @endif
                         </div>
                     </div>
                 </div>
